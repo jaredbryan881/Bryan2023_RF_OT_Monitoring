d211c41c8ea9e2184f65b27894bc4881aaadc794
Jared Bryan <jared.bryan1998@gmail.com>
2022-03-07 14:26:50 -0500

    Added 1d distance matrix for building GSOT distance matrices

--- distribution_utils.py
+++ distribution_utils.py
@@ -56,7 +56,26 @@ def rf_hist(rf_arr, grid):
 
 	return hist
 
-def distance_matrix(grid, t_weight=1.0, norm=True):
+def distance_matrix_1d(arr1, arr2):
+	"""
+	Calculate pairwise distances between two arrays, using the euclidean distance
+
+	Arguments
+	---------
+	:param arr1: np.array
+		First signal
+	:param arr2: np.array
+		Second signal
+
+	Returns
+	-------
+	:return M: np.array
+		Distance matrix
+	"""
+	M = cdist(arr1, arr2, metric='euclidean')
+	return M
+
+def distance_matrix_2d(grid, t_weight=1.0, norm=True):
 	"""
 	Calculate the pairwise distance between each point on the time-amplitude grid.
 
@@ -79,13 +98,10 @@ def distance_matrix(grid, t_weight=1.0, norm=True):
 		T=T/np.max(np.abs(T))
 		Y=Y/np.max(np.abs(Y))
 
-	# compute pairwise distance between points on a 2d grid
-	coords = np.array([t_weight*T.flatten(), Y.flatten()]).T
-
 	T_coords = T.flatten()[...,np.newaxis]
 	Y_coords = Y.flatten()[...,np.newaxis]
-	M_t = cdist(T_coords,T_coords, metric='euclidean')
-	M_a = cdist(Y_coords,Y_coords, metric='euclidean')
+	M_t = distance_matrix_1d(T_coords, T_coords)
+	M_a = distance_matrix_1d(Y_coords, Y_coords)
 	M = M_t + t_weight*M_a
 	M/=np.sum(M)
 
