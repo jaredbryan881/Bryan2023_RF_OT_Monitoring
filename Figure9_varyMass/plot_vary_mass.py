6b1ced7ed80beece789203bc485e5d8d9c9d9ac1
Jared Bryan <jared.bryan1998@gmail.com>
2022-03-24 09:39:19 -0400

    Added varyNoise and varyMass appendix figs

---
+++ Figure9_varyMass/plot_vary_mass.py
@@ -0,0 +1,106 @@
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from telewavesim import utils as ut
+
+import ot
+
+import sys
+sys.path.append("../")
+from sim_synth import simulate_RF
+from distance_matrix import distance_matrix_1d
+from ot_distance import partial_ot_dist_tlp, ot_dist_tlp
+
+def main():
+	# Show the transport map for a range of masses
+
+	# define parameters
+	modfile = '../velocity_models/model_lohs.txt'
+	wvtype = 'P' # incident wave type
+	npts = 8193  # Number of samples
+	dt = 0.05
+	baz = 0.0    # Back-azimuth direction in degrees (has no influence if model is isotropic)
+	
+	t_axis = np.linspace(-(npts//2)*dt, (npts//2)*dt, npts) # time axis
+	tlim = [-1.0, 10.0] # time window
+	t_inds = (t_axis >= tlim[0]) & (t_axis < tlim[1]) # corresponding indices
+	npts_win = np.sum(t_inds)
+	flim = 1.0 # bandpass frequencies
+	slow = 0.05 # slowness
+	pert = -0.02
+	masses = [0.9,0.95]
+
+	save_fig=False
+
+	# load model
+	ref_model = ut.read_model(modfile)
+
+	# simulate default RFs at a range of slowness values
+	print("Generating reference distribution")
+	rf_ref = simulate_RF(ref_model, slow, baz, npts, dt, freq=flim, vels=None).data
+
+	delta_t = np.max(t_axis[t_inds])-np.min(t_axis[t_inds])
+	delta_a = np.max(rf_ref)-np.min(rf_ref)
+	t_weight = delta_t/delta_a
+
+	pert_model = copy.deepcopy(ref_model)
+	pert_model.vs[0]*=(1+pert)
+	pert_model.update_tensor()
+
+	# Calculate the OT distance for the full distribution
+	# simulate default RFs at a range of slowness values
+	rf_pert = simulate_RF(pert_model, slow, baz, npts, dt, freq=flim, vels=None).data
+
+	M_t = distance_matrix_1d(t_axis[t_inds,np.newaxis], t_axis[t_inds,np.newaxis])
+	M_a = distance_matrix_1d(rf_ref[t_inds,np.newaxis], rf_pert[t_inds,np.newaxis])
+	M_tlp = M_t + t_weight*M_a
+
+	# Individual RFs
+	fig,axs=plt.subplots(3,1,sharex=True,sharey=True, figsize=(15,8))
+
+	for (m,mass) in enumerate(masses):
+		print("--Mass={}".format(mass))
+
+		# Stacked TLp
+		ot_map = partial_ot_dist_tlp(M_tlp, m=mass)
+
+		# source distribution and transport vectors
+		axs[m].scatter(t_axis[t_inds], rf_ref[t_inds], c='crimson', ec='k', s=40)
+		axs[m].scatter(t_axis[t_inds], rf_pert[t_inds], c='steelblue', ec='k', s=40)
+		# iterate over times in first RF dist
+		for i in range(npts_win):
+			if np.sum(ot_map[i]==1)!=0:
+				# coords of the source point
+				vector_x = t_axis[t_inds][ot_map[i]==1]-t_axis[t_inds][i]
+				vector_y = rf_pert[t_inds][ot_map[i]==1]-rf_ref[t_inds][i]
+				axs[m].plot([t_axis[t_inds][i], t_axis[t_inds][i]+vector_x], [rf_ref[t_inds][i], rf_ref[t_inds][i]+vector_y], c='k')
+
+	# Full OT for mass=1.0
+	print("--Mass=1.0")
+	_,ot_map = ot_dist_tlp(M_tlp)
+	dists = np.mean(ot_map*M_tlp,axis=0)
+	# source distribution and transport vectors
+	axs[len(masses)].scatter(t_axis[t_inds], rf_ref[t_inds], c='crimson', ec='k', s=40)
+	axs[len(masses)].scatter(t_axis[t_inds], rf_pert[t_inds], c='steelblue', ec='k', s=40)
+	# iterate over times in first RF dist
+	for i in range(npts_win):
+		if np.sum(ot_map[i]==1)!=0:
+			# coords of the source point
+			vector_x = t_axis[t_inds][ot_map[i]==1]-t_axis[t_inds][i]
+			vector_y = rf_pert[t_inds][ot_map[i]==1]-rf_ref[t_inds][i]
+			axs[len(masses)].plot([t_axis[t_inds][i], t_axis[t_inds][i]+vector_x], [rf_ref[t_inds][i], rf_ref[t_inds][i]+vector_y], c='k')
+
+	plt.subplots_adjust(hspace=0)
+	plt.tight_layout()
+	axs[0].set_xlim(-1,10)
+
+	if save_fig:
+		plt.savefig("ot_match_{}.pdf".format(mass))
+		plt.savefig("ot_match_{}.png".format(mass))
+	plt.show()
+
+
+if __name__=="__main__":
+	main()
