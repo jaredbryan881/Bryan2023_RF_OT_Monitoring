d8e986b991b5005460a67db6e4534fa1f12a5272
Jared Bryan <jared.bryan1998@gmail.com>
2022-03-24 09:39:02 -0400

    Added full tlp distance

--- ot_distance.py
+++ ot_distance.py
@@ -1,6 +1,36 @@
 import numpy as np
 import ot
 
+def ot_dist_tlp(M, dist1=None, dist2=None):
+	"""
+	Arguments
+	---------
+	:param M: np.array
+		Distance matrix
+	:param dist1: np.array
+		Source distribution. If left unspecified, assume uniform distribution.
+	:param dist2: np.array
+		Target distribution. If left unspecified, assume uniform distribution.
+
+	Returns
+	-------
+	:return dist: float
+		Transport Lp distance
+	:return ot_map: np.array
+		Optimal transport map
+	"""
+	# calculate optimal mapping
+	if dist1 is None:
+		dist1 = np.ones(M.shape[0])
+	if dist2 is None:
+		dist2 = np.ones(M.shape[1])
+	ot_map = ot.emd(dist1, dist2, M, numItermax=500000)
+
+	# calculate distance
+	dist = np.sum(ot_map*M,axis=0)
+
+	return dist, ot_map
+
 def partial_ot_dist_tlp(M, dist1=None, dist2=None, m=0.99, numItermax=100000, nb_dummies=1):
 	"""
 	Arguments
